26e84efb648c645f6a7de959e98c9ae71acd622d
ShaikMuktharBasha <2300032055@kluniversity.in>
2025-07-26 20:24:23 +0530

    Fix: Set Vite base for Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/S219_CICD/', // <-- Add this line
   plugins: [react()],
-  base: '/S219_CICD/'
 });
